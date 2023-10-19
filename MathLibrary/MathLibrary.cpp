05a07c108d7c2a040aa19cca96cd2ceea8d24fc5
1478 <1478@L1299>
2023-10-19 08:49:01 +0530

    Add project files.

---
+++ MathLibrary/MathLibrary.cpp
@@ -0,0 +1,9 @@
+#include "MathLibrary.h"
+namespace MathLibrary {
+	int Arithmetic::add(int a, int b) {
+		return a + b;
+	}
+	int Arithmetic::sub(int a, int b) {
+		return a - b;
+	}
+}
---
+++ MathLibrary/MathLibrary.h
@@ -0,0 +1,8 @@
+#pragma once
+namespace MathLibrary {
+	class Arithmetic {
+	public:
+		static int add(int a, int b);
+		static int sub(int a, int b);
+	};
+}
